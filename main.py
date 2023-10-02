e94a9d7e1c3d2609b6cdede199a90b16a63fe9f1
Shivangi varshney <132803861+Shivangii13@users.noreply.github.com>
2023-10-02 19:16:16 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+row1 = ["😀" , "🥹" , "😁"]
+row2 = ["😁" , "😆" , "😅"]
+row3 = ["😍" , "🥰" , "😘"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("where do you want tp put the treasure?")
+
+horizontal = int(position[0])
+vertical = int(position[1])
+
+selected_row = map[vertical -1]
+selected_row[horizontal -1] = 's'
+
+
+print(selected_row)
+
